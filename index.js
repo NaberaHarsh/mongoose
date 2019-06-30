40769da7b05af43765a851d73456afd093d7883d
Harsh Nabera <harshnabera30@gmail.com>
2019-06-30 23:04:43 +0530

    blog

--- index.js
+++ index.js
@@ -1,66 +1,125 @@
-const express=require("express")
-const bodyParser=require("body-parser");
-const mongoose=require("mongoose");
-const Schema=mongoose.Schema;
+// const express=require("express")
+// const bodyParser=require("body-parser");
+// const mongoose=require("mongoose");
+// const Schema=mongoose.Schema;
+// const cors=require("cors")
 
-const server=express();
-server.use(bodyParser.json());
-server.use(bodyParser.urlencoded());
+// const server=express();
+// server.use(bodyParser.json());
+// server.use(bodyParser.urlencoded());
+// server.use(cors());
 
-mongoose.connect('mongodb://localhost:27017/test')
+// mongoose.connect('mongodb://localhost:27017/todo')
 
-const taskSchema= new Schema({
-title:String,
-status:Boolean,
-age: {type:Number, default:18, min:18, max:65},
-date:{type:Date, default: Date.now}
-})
+// const taskSchema= new Schema({
+// name:String,
+// status:Boolean,
+// date:{type:Date, default: Date.now}
+// })
 
-const Task=mongoose.model('Task',taskSchema);
+// const Task=mongoose.model('Task',taskSchema);
 
-server.post("/task",function(req,res){
+// server.post("/task",function(req,res){
 
-    let  task= new Task();
+//     let  task= new Task();
 
-    task.title=req.body.title;
-    task.status=true;
-    task.age=18;
-    task.date=new Date();
+//     task.name=req.body.name;
+//     task.status=req.body.status;
+//         task.date=new Date();
 
-    console.log(task);
-    task.save();
-    res.json(task);
+//     console.log(task);
+//     task.save();
+//     res.json(task);
 
-})
+// })
 
-server.get("/tasks/:name",function(req,res){
-Task.findOne({title:req.params.name}, function(err,doc){
-    console.log(doc);
-    res.json(doc);
-})
-})
+// server.get("/tasks/:name",function(req,res){
+// Task.findOne({title:req.params.name}, function(err,doc){
+//     console.log(doc);
+//     res.json(doc);
+// })
+// })
 
-server.get("/read",function(req,res){
-    Task.find({},function(err,doc){
-        console.log(doc);
+// server.get("/read",function(req,res){
+//     Task.find({},function(err,doc){
+//         console.log(doc);
+//         res.json(doc);
+               
+//     })
+// })
+
+// server.put("/update/:name",(req,res)=>{
+//     Task.findOneAndUpdate({_id:req.params.name},{status:req.body.status},function(err,doc){
+//         console.log(doc);
+//         res.json(doc);
+//     })
+// })
+
+// server.delete("/delete/:name",(req,res)=>{
+// Task.findOneAndDelete({_id:req.params.name},function(err,doc){
+//     console.log(doc);
+//     res.json(doc);
+// })
+// })
+
+// server.listen(8080,()=>{
+//     console.log("server has started")
+// })
+
+
+
+// Blog starts here
+
+const express = require("express");
+const server = express();
+const bodyParser = require("body-parser");
+const cors = require('cors');
+
+
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+mongoose.connect('mongodb://localhost:27017/blog', {useNewUrlParser: true});
+
+
+const blogSchema = new Schema({
+    title:  {type:String,required:true},
+    desc:  {type:String,required:true},
+    image:  {type:String,required:true},   
+    uid:  {type:String,required:true}
+  });
+
+const Blog = mongoose.model('Blog', blogSchema);
+
+
+server.use(bodyParser.urlencoded())
+server.use(bodyParser.json())
+server.use(cors());
+
+
+server.post("/blog",function(req,res){
+    
+    let b = new Blog();
+    b.title = req.body.title;
+    b.desc = req.body.desc;
+    b.image = req.body.image;
+    b.uid = req.body.uid;
+    b.save().then((doc)=>{
         res.json(doc);
     })
-})
 
-server.put("/update/:name",(req,res)=>{
-    Task.findOneAndUpdate({title:req.params.name},{title:"WOPAHH!!"},{overwrite:true},function(err,doc){
-        console.log(doc);
-        res.json(doc);
-    })
-})
+})   
 
-server.delete("/delete/:name",(req,res)=>{
-Task.findOneAndDelete({title:req.params.name},function(err,doc){
-    console.log(doc);
-    res.json(doc);
-})
-})
+server.get("/blogs",function(req,res){
+    
+ Blog.find({uid:req.query.uid}).then((docs)=>{
+    res.json(docs);
+ })
 
-server.listen(8080,()=>{
-    console.log("server has started")
+
+})   
+
+
+server.listen(8080,function(){
+    console.log("server started");
 })
